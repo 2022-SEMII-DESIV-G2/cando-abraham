afe9b6b0d1e4748805c877770548701487f10f3e
ProgramadorPTY <abrahamcito1932@gmail.com>
2022-09-14 12:24:29 -0500

    1:agregando laboratorio4

---
+++ laboratorio4/main.js
@@ -0,0 +1,6 @@
+function enviar(){
+  var correo=document.getElementById("correoelectronico").value;
+  var contraseña=document.getElementById("password").value;
+  console.log("correo: "+correo);
+  console.log("contraseña: "+contraseña);
+}
